9fa8ff8e82c07264019a2f97f8127715db0c44ab
soyoon247 <skyksy0655@gmail.com>
2022-07-01 09:05:20 +0900

    [admin] 어드민 필드 수정

--- food/admin.py
+++ food/admin.py
@@ -4,7 +4,7 @@ from food.models import FoodPrice
 
 # Register your models here.
 class FoodPricdAdmin(admin.ModelAdmin):
-	...
+	list_display = ('city_code', 'amount', 'updated_at', )
 
 
 admin.site.register(FoodPrice, FoodPricdAdmin)
--- hotels/admin.py
+++ hotels/admin.py
@@ -4,7 +4,7 @@ from hotels.models import HotelPrice
 
 # Register your models here.
 class HotelPriceAdmin(admin.ModelAdmin):
-	list_display = ('city_code', 'check_in_date', "min_price", "max_price", "median_price",)
+	list_display = ('city_code', 'check_in_date', "min_price", "max_price", "median_price", 'updated_at', )
 
 
 admin.site.register(HotelPrice, HotelPriceAdmin)
