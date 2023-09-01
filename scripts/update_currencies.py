e17e8e06f069c2dd128eaceea93139d8c92e8d5f
soyoon <soyoon.dev@gmail.com>
2023-09-01 18:44:28 +0900

    currency 업데이트 스크립트 추가

---
+++ scripts/update_currencies.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/travel_calculator")
+sys.path.append(BASE_DIR)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "calculator.settings")
+load_dotenv(
+    os.path.join(BASE_DIR, '.env')
+)
+
+import django
+django.setup()
+
+from currencies.services import CurrencyScheduleManager
+
+if __name__ == '__main__':
+    CurrencyScheduleManager().execute()
