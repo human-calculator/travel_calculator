77a4e2d3c513939951b48d7d4e784bcba1ad2484
soyoon <soyoon.dev@gmail.com>
2023-09-27 02:04:07 +0900

    DJANGO_SETTINGS_MODULE 수정

--- scripts/update_test.py
+++ scripts/update_test.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/travel_calculator")
 sys.path.append(BASE_DIR)
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "calculator.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "calculator.settings.base")
 load_dotenv(
     os.path.join(BASE_DIR, '.env')
 )
